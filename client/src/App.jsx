0f33dfe92df159f5979ae11b1d3eba404b244a3f
Prashant <prashant@example.com>
2024-12-13 13:52:19 -0500

    add website updates

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import "./App.css";
 
 export default function App() {
   return (
-    <div>
-      Rasilo MoMo
-      <div>
-        Coming Soon..
-      </div>
-      <div>
-        what's up everybody
+    <div className="app-container">
+      <h1>Rasilo MoMo</h1>
+      <p>Coming Soon...</p>
+      <div className="order-section">
+        <p>In the meantime, you can place an order using the options below:</p>
+        <ul>
+          <li>
+            <a href="https://www.instagram.com/rasilo_momo/" target="_blank" rel="noopener noreferrer">
+              Order via Instagram
+            </a>
+          </li>
+          <li>
+            <a href="tel:+16476762455">Call Us: +1 647-676-2455</a>
+          </li>
+        </ul>
       </div>
     </div>
-  )
+  );
 }
